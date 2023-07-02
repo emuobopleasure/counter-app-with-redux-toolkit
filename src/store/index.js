622976aefd3e0b909028eb51e0e4f03918d3d233
Davis06 <davis06@outlook.com>
2023-07-02 07:24:58 +0100

    Implemented store creation using configureStore and introduced state slices through createSlice.

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+import { createStore } from 'redux'
+
+const initialState = { counter: 0, showCounter: true }
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state) {
+            state.counter ++
+        },
+        decrement(state) {
+            state.counter --
+        },
+        increase(state, action) {
+            state.counter = state.counter + action.amount
+        },
+        toggleCounter(state) {
+            state.showCounter = !state.showCounter
+        }
+    }
+})
+
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
+
+export default store
